8a69d0cda64f1c5a9589fa04b56953d44cb6e722
RocioEnriquez22 <rociocenriquez@gmail.com>
2026-08-18 22:56:49 -0300

    commit para no perder todo

--- js/main.js
+++ js/main.js
@@ -2,14 +2,31 @@ const noaGroup = document.querySelector('g.grupo-noroeste').closest('a');
 const infoBox = document.getElementById('info-region');
 const titulo = document.getElementById('titulo-region');
 const descripcion = document.getElementById('descripcion-region');
+const anchoPantalla = window.innerWidth;
+const contenedorMapa = document.getElementById('contenedor-mapa');
+const esTactil = window.matchMedia("(hover:none)").matches;
 
+console.log(anchoPantalla);
+const anchoPermitido = 1060;
 
+if (anchoPantalla < anchoPermitido) { infoBox.remove(); }
+
+if (esTactil) {
+    infoBox.remove();
+    contenedorMapa.style.justifyContent = 'center';
+
+}
+
+//../imgs/pqCalilegua/carrusel_pn_calilegua_04.jpg'
 noaGroup.addEventListener('mouseenter', () => {
-    infoBox.style.display = 'flex';
-    infoBox.style.flex='1';
-    infoBox.style.objectFit='cover';    infoBox.style.backgroundImage = "url('../imgs/pqCalilegua/carrusel_pn_calilegua_04.jpg')";
-    titulo.textContent = "Región NOA";
-    descripcion.textContent = "El Noroeste Argentino destaca por sus paisajes andinos, cerros de colores y una rica cultura ancestral.";
+    if (anchoPantalla >= anchoPermitido) {
+        infoBox.style.display = 'flex';
+        infoBox.style.flex = '1';
+        infoBox.style.objectFit = 'cover';
+        infoBox.style.backgroundImage = "url('./imgs/pqCalilegua/carrusel_pn_calilegua_04.jpg')";
+        titulo.textContent = "Región NOA";
+        descripcion.textContent = "El Noroeste Argentino destaca por sus paisajes andinos, cerros de colores y una rica cultura ancestral.";
+    }
 });
 
 noaGroup.addEventListener('mouseleave', () => {
@@ -17,77 +34,86 @@ noaGroup.addEventListener('mouseleave', () => {
 });
 
 const neaGroup = document.querySelector('g.grupo-noreste');
-neaGroup.addEventListener('mouseleave', ()=>{
-    infoBox.style.display='none';
+neaGroup.addEventListener('mouseleave', () => {
+    infoBox.style.display = 'none';
 }
 );
 
 neaGroup.addEventListener('mouseenter', () => {
-    infoBox.style.display = 'flex';
-    infoBox.style.flex='1';
-    infoBox.style.background = "grey";
-    titulo.textContent = "UPS :(";
-    descripcion.textContent = "Estamos trabajando en estas paginas";
-    descripcion.style.textAlign='center';
+    if (anchoPantalla >= anchoPermitido) {
+        infoBox.style.display = 'flex';
+        infoBox.style.flex = '1';
+        infoBox.style.background = "grey";
+        titulo.textContent = "UPS :(";
+        descripcion.textContent = "Estamos trabajando en estas paginas";
+        descripcion.style.textAlign = 'center';
+
+    }
 });
 
 const centroGroup = document.querySelector('g.grupo-centro');
-centroGroup.addEventListener('mouseleave', ()=>{
-    infoBox.style.display='none';
+centroGroup.addEventListener('mouseleave', () => {
+    infoBox.style.display = 'none';
 }
 );
 
 centroGroup.addEventListener('mouseenter', () => {
-    infoBox.style.display = 'flex';
-    infoBox.style.flex='1';
-    infoBox.style.background = "grey";
-    titulo.textContent = "UPS :(";
-    descripcion.textContent = "Estamos trabajando en estas paginas";
-    descripcion.style.textAlign='center';
+    if (anchoPantalla >= anchoPermitido) {
+        infoBox.style.display = 'flex';
+        infoBox.style.flex = '1';
+        infoBox.style.background = "grey";
+        titulo.textContent = "UPS :(";
+        descripcion.textContent = "Estamos trabajando en estas paginas";
+        descripcion.style.textAlign = 'center';
+    }
 });
 
 const patagoniaGroup = document.querySelector('g.grupo-patagonia');
-patagoniaGroup.addEventListener('mouseleave', ()=>{
-    infoBox.style.display='none';
+patagoniaGroup.addEventListener('mouseleave', () => {
+    infoBox.style.display = 'none';
 }
 );
 
 patagoniaGroup.addEventListener('mouseenter', () => {
-    infoBox.style.display = 'flex';
-    infoBox.style.flex='1';
-    infoBox.style.background = "grey";
-    titulo.textContent = "UPS :(";
-    descripcion.textContent = "Estamos trabajando en estas paginas";
-    descripcion.style.textAlign='center';
+    if (anchoPantalla >= anchoPermitido) {
+        infoBox.style.display = 'flex';
+        infoBox.style.flex = '1';
+        infoBox.style.background = "grey";
+        titulo.textContent = "UPS :(";
+        descripcion.textContent = "Estamos trabajando en estas paginas";
+        descripcion.style.textAlign = 'center';
+    }
 });
 
-const pataAustralGroup= document.querySelector('g.grupo-patagoniaAustral');
-patagoniaAustralGroup.addEventListener('mouseleave', ()=>{
-    infoBox.style.display='none';
+const pataAustralGroup = document.querySelector('g.grupo-patagoniaAustral');
+pataAustralGroup.addEventListener('mouseleave', () => {
+    infoBox.style.display = 'none';
 }
 );
 
-patagoniaAustralGroup.addEventListener('mouseenter', () => {
-    infoBox.style.display = 'flex';
-    infoBox.style.flex='1';
-    infoBox.style.background = "grey";
-    titulo.textContent = "UPS :(";
-    descripcion.textContent = "Estamos trabajando en estas paginas";
-    descripcion.style.textAlign='center';
+pataAustralGroup.addEventListener('mouseenter', () => {
+    if (anchoPantalla >= anchoPermitido) {
+        infoBox.style.display = 'flex';
+        infoBox.style.flex = '1';
+        infoBox.style.background = "grey";
+        titulo.textContent = "UPS :(";
+        descripcion.textContent = "Estamos trabajando en estas paginas";
+        descripcion.style.textAlign = 'center';
+    }
 });
 
 function mostrarInfo(event) {
-    // Si la pantalla es menor a 768px, NO ejecutar el código de ocultar/mostrar
-    if (window.innerWidth <= 768) {
-        return; 
+    // Si la pantalla
+    if (window.innerWidth > anchoPermitido) {
+        // Aquí va tu código actual que muestra/oculta el div
+        const infoDiv = document.getElementById('info-region');
+        //infoDiv.style.display = 'block';
+        infoBox.style.display = 'flex';
+        infoBox.style.flex = '1';
+        infoBox.style.objectFit = 'cover'; infoBox.style.backgroundImage = "url('../imgs/pqCalilegua/carrusel_pn_calilegua_04.jpg')";
+        titulo.textContent = "Región NOA";
+        descripcion.textContent = "El Noroeste Argentino destaca por sus paisajes andinos, cerros de colores y una rica cultura ancestral.";
+        ;
     }
 
-    // Aquí va tu código actual que muestra/oculta el div
-    const infoDiv = document.getElementById('info-region');
-    //infoDiv.style.display = 'block';
-    infoBox.style.display = 'flex';
-    infoBox.style.flex='1';
-    infoBox.style.objectFit='cover';    infoBox.style.backgroundImage = "url('../imgs/pqCalilegua/carrusel_pn_calilegua_04.jpg')";
-    titulo.textContent = "Región NOA";
-    descripcion.textContent = "El Noroeste Argentino destaca por sus paisajes andinos, cerros de colores y una rica cultura ancestral.";
 }
